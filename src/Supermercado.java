6ad16201d4896e2787e80fce7d6ad9515a669bcd
valentinacorream <valentina.corream@uqvirtual.edu.co>
2026-09-23 21:02:25 -0500

    Creada la primera parte de la clase Supermercado

---
+++ src/Supermercado.java
@@ -0,0 +1,42 @@
+public class Supermercado {
+    private String nombre;
+    private String direccion;
+    private int telefono;
+
+    public Supermercado(String nombre, String direccion, int telefono) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre+
+                ", Direccion: " + direccion+
+                ", Telefono: " + telefono;
+    }
+}
