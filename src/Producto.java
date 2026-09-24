1dfc6f83c854ec498eeafc0e5c4d52e09775bc22
valentinacorream <valentina.corream@uqvirtual.edu.co>
2026-09-23 21:31:57 -0500

    Creada la primera parte de la clase Producto

---
+++ src/Producto.java
@@ -0,0 +1,54 @@
+public class Producto {
+
+    private String codigo;
+    private String nombre;
+    private double precioUnitario;
+    private int cantidadDisponible;
+
+    public Producto(String codigo, String nombre, double precioUnitario, int cantidadDisponible) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precioUnitario = precioUnitario;
+        this.cantidadDisponible = cantidadDisponible;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public void setPrecioUnitario(double precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
+    public int getCantidadDisponible() {
+        return cantidadDisponible;
+    }
+
+    public void setCantidadDisponible(int cantidadDisponible) {
+        this.cantidadDisponible = cantidadDisponible;
+    }
+
+    @Override
+    public String toString() {
+        return "Codigo: " + codigo +
+                ", Nombre: " + nombre +
+                ", Preci oUnitario: " + precioUnitario +
+                ", Cantidad Disponible=" + cantidadDisponible;
+    }
+}
